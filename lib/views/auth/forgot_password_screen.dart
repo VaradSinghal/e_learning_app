3c7854aa74ce423f11b6cc18f4932d68662621f5
VaradSinghal <varadsinghal1526@gmail.com>
2025-04-12 21:11:17 +0530

    forgotpass screen

--- lib/views/auth/forgot_password_screen.dart
+++ lib/views/auth/forgot_password_screen.dart
@@ -1,13 +1,125 @@
+import 'package:e_learning_app/core/utils/validators.dart';
+import 'package:e_learning_app/widgets/common/custom_button.dart';
+import 'package:e_learning_app/widgets/common/custom_textfield.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class ForgotPasswordScreen extends StatelessWidget {
+class ForgotPasswordScreen extends StatefulWidget {
   const ForgotPasswordScreen({super.key});
+
+  @override
+  State<ForgotPasswordScreen> createState() => _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends State<ForgotPasswordScreen> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    super.dispose();
+  }
+
+  void _handleResetPassword() {
+    if (_formKey.currentState!.validate()) {
+      // Perform the password reset logic here
+      Get.dialog(AlertDialog(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(20),
+        ),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Icon(
+              Icons.mark_email_read_outlined,
+              size: 60,
+              color: Colors.green,
+            ),
+
+             SizedBox(height: 20),
+
+             Text(
+                'Check your email',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
+                ),
+             ),
+            SizedBox(height: 10),
+              Text(
+                'We have sent you a password reset instructions to your email.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.grey,
+                ),
+              ),
+
+              SizedBox(height: 20),
+              CustomButton(
+                text: 'OK',
+                onPressed: () => Get.back(),
+                height: 55,
+               
+                
+              ),
+          ],
+        ),
+      ),
+      );
+      
+      
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        title:  Text('Forgot Password'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () => Get.back(),
+          icon: const Icon(Icons.arrow_back_ios, color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Forgot Password',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            Text(
+              'Enter your email address to reset your password',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+            ),
+
+            const SizedBox(height: 10),
+            Form(
+              key: _formKey,
+              child: CustomTextfield(
+                label: 'Email',
+                prefixIcon: Icons.email_outlined,
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                validator: FormValidator.validateEmail,
+              ),
+            ),
+
+            const SizedBox(height: 20),
+            CustomButton(text: 'Reset Password', onPressed: _handleResetPassword),
+          ],
+        ),
       ),
     );
   }
