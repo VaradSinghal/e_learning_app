576d08deb4929a156fbb53b284732fcf5f7262f7
VaradSinghal <varadsinghal1526@gmail.com>
2025-04-14 19:37:45 +0530

    profile-screen

--- lib/views/profile/profile_screen.dart
+++ lib/views/profile/profile_screen.dart
@@ -1,3 +1,7 @@
+import 'package:e_learning_app/core/theme/app_colors.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_app_bar.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_options.dart';
+import 'package:e_learning_app/views/profile/widgets/profile_stats_card.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatelessWidget {
@@ -5,9 +9,28 @@ class ProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Profile'),
+    return const Scaffold(
+      backgroundColor: AppColors.lightBackground,
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          ProfileAppBar(
+            initials: 'JD',
+            fullName: 'John Doe',
+            email: 'johndoe@gmail.com',
+          ),
+          SliverToBoxAdapter(
+            child: Padding(padding: EdgeInsets.all(20),
+              child: Column(
+                  children: [
+                    ProfileStatsCard(),
+                    SizedBox(height: 24),
+                    ProfileOptions(),
+                  ],
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
